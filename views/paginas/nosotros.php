f075737954f248a6f4d0075243c61c83842ec5fc
Alexanderel04 <2004.estrada.lopez@gmail.com>
2023-04-13 11:36:54 -0600

    "Septimo Update"

---
+++ views/paginas/nosotros.php
@@ -0,0 +1,24 @@
+<div class="contenedor">
+    <div class="contenedor-nosotros">
+        <div class="nosotros">
+            <h2>Quienes somos?</h2>
+            <p>Somos una empresa desarrolladora de sitios web y proximamente de aplicaciones para cualquier negocio o empresa, tratamos de dar el mejor servicio a nuestros usuarios. </p>
+            <p>Estamos dedicados al desarrollo, y buscamos mejorar cada vez mas para buscar una mejor satisfaccion, y una mejor calidad de servicio y de software.</p>
+            <div class="nosotros-boton">
+                <a href="/contacto" class="boton-contacto">Contactenos</a>
+            </div>
+        </div>
+        <div class="tarjeta-nosotros">
+            <div class="servicio-info">
+                <h3>Ofrecemos</h3>
+                <ul class="lista">
+                    <li type="disc">Desarrollo de sitios web</li>
+                    <li type="disc">Desarrollo de tiendas online</li>
+                    <li type="disc">Mantenimiento gratuito el primer ano</li>
+                    <li type="disc">Dominio gratuito por un ano</li>
+                    <li type="disc">Precios justos y transparentes</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
